2797c5b5f7d80481e901d968d1c7887e9c18be1f
Hui Wang <hui@podme.com>
2025-03-02 22:48:09 +0100

    ContentTransitionDemo

---
+++ ContentTransitionDemo/ContentTransitionDemo/ContentTransitionDemo/ContentTransitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentTransitionDemoApp.swift
+//  ContentTransitionDemo
+//
+//  Created by Hui Wang on 2025-03-02.
+//
+
+import SwiftUI
+
+@main
+struct ContentTransitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentTransitionDemo/ContentTransitionDemo/ContentTransitionDemo/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  ContentTransitionDemo
+//
+//  Created by Hui Wang on 2025-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPlaying = false
+    
+    var body: some View {
+        VStack {
+            Button {
+                withAnimation {
+                    isPlaying.toggle()
+                }
+            } label: {
+                Label("Play/Pause", systemImage: isPlaying ? "pause.fill" : "play.fill")
+            }
+            .contentTransition(.symbolEffect(.replace))
+        }
+        .padding()
+        .font(.title)
+    }
+}
+
+#if DEBUG
+#Preview {
+    ContentView()
+}
+#endif
